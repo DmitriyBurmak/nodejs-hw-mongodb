fc956692de45954ba690d7e487e0fefe612c30b7
Дмитрий Бурмак <97616159+DmitriyBurmak@users.noreply.github.com>
2025-08-09 12:32:43 +0300

    fix errorHandlers

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,9 +1,18 @@
-export const errorHandler = (err, req, res, next) => {
-  const { status = 500, message = 'Something went wrong', data = {} } = err;
+import { HttpError } from 'http-errors';
 
-  res.status(status).json({
-    status,
-    message,
-    data,
+export const errorHandler = (err, req, res, next) => {
+  if (err instanceof HttpError) {
+    res.status(err.status).json({
+      status: err.status,
+      message: err.name,
+      data: err,
+    });
+    return;
+  }
+
+  res.status(500).json({
+    status: 500,
+    message: 'Something went wrong',
+    data: err.message,
   });
 };
