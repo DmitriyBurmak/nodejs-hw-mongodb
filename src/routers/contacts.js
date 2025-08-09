b823496cc5586c7afd397742616c451a39d2c31e
Дмитрий Бурмак <97616159+DmitriyBurmak@users.noreply.github.com>
2025-08-09 12:20:21 +0300

    fix router.post

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -14,7 +14,7 @@ router.get('/contacts', ctrlWrapper(getContactsController));
 
 router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
 
-router.post('/contscts', ctrlWrapper(createContactController));
+router.post('/contacts', ctrlWrapper(createContactController));
 router.patch('/contacts/:contactId', ctrlWrapper(patchContactController));
 router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
 export { router as contactsRouter };
