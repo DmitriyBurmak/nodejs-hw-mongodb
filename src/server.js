ce6b09a80cffb355777ab0f807291c7b525a74d4
Дмитрий Бурмак <97616159+DmitriyBurmak@users.noreply.github.com>
2025-08-10 09:40:54 +0300

    adjustments server.js

--- src/server.js
+++ src/server.js
@@ -12,6 +12,7 @@ export function setupServer() {
   const PORT = Number(process.env.PORT) || 3000;
 
   app.use(cors());
+  app.use(express.json());
 
   app.use((req, res, next) => {
     logger.info({ method: req.method, url: req.url, body: req.body });
